8be49d382d86f91ae650c6e5b8acd69d9610b7a7
Natasha <admin@natashaosborne.com>
2022-01-14 01:28:44 -0700

    update addon name

--- EditMacroTargets.lua
+++ EditMacroTargets.lua
@@ -1,15 +1,14 @@
 -- ---------------------------------
 -- Bulk Edit Targets
--- Originally by cptsamcarter
+-- Originally by omadasala
 
-SLASH_BulkEditMacroTargets1 = "/bet";
+SLASH_EditMacroTargets1 = "/bet";
 
 -- TODOS
 -- if either from or to is empty display an error
   -- on button click
   -- when using the replace method
 -- configure help tips
--- change the name to EditMacroTargets? (Github, files etc)
 -- update slash commands (bet or betm, and when it is written out in long form)
 -- is there a better way to get the first index of the macros, or get the list to map over?
 -- refactor/cleanup
@@ -44,7 +43,7 @@ local function UpdateMacro(from, to, previousIndex)
 end
 
 local function DisplayModal(msg)
-    local Modal = CreateFrame("Frame", "BET_TargetFrame", UIParent, "EtherealFrameTemplate");
+    local Modal = CreateFrame("Frame", "EMT_TargetFrame", UIParent, "EtherealFrameTemplate");
 
     Modal:SetSize(300, 360);
     Modal:SetPoint("CENTER", UIParent, "CENTER");
@@ -55,7 +54,7 @@ local function DisplayModal(msg)
     Modal.title:SetPoint("CENTER", Modal.TitleBg, "CENTER", 5, 0);
     Modal.title:SetText("Bulk Edit Target Options");
 
-    Modal.saveButton = CreateFrame("Button", "saveButton", Modal, "BEMT_ButtonTemplate");
+    Modal.saveButton = CreateFrame("Button", "saveButton", Modal, "EMT_ButtonTemplate");
     Modal.saveButton:SetPoint("CENTER", Modal, "Bottom", 0, 70);
     Modal.saveButton:SetSize(70, 30);
     Modal.saveButton:SetText("Save");
@@ -69,7 +68,7 @@ local function DisplayModal(msg)
         HideUIPanel(Modal);
     end)
 
-    Modal.editInput1 = CreateFrame("EditBox", "FromInput", Modal, "BEMT_InputBoxTemplate");
+    Modal.editInput1 = CreateFrame("EditBox", "FromInput", Modal, "EMT_EditBoxTemplate");
     Modal.editInput1:SetPoint("LEFT", Modal, "TOP", -60, -100);
     Modal.editInput1:SetWidth(180);
     Modal.editInput1:SetHeight(40);
@@ -77,7 +76,7 @@ local function DisplayModal(msg)
     Modal.editInput1:SetAutoFocus(true);
     Modal.editInput1:SetMaxLetters(100);
 
-    Modal.editInput2 = CreateFrame("EditBox", "ToInput", Modal, "BEMT_InputBoxTemplate");
+    Modal.editInput2 = CreateFrame("EditBox", "ToInput", Modal, "EMT_EditBoxTemplate");
     Modal.editInput2:SetPoint("LEFT", Modal,  "TOP", -60, -130);
     Modal.editInput2:SetWidth(180);
     Modal.editInput2:SetHeight(40);
@@ -86,7 +85,7 @@ local function DisplayModal(msg)
     Modal.editInput2:SetMaxLetters(100);
 end
 
-SlashCmdList["BulkEditMacroTargets"] = function(msg)
+SlashCmdList["EditMacroTargets"] = function(msg)
     local command, rest = msg:match("^(%S*)%s*(.-)$");
 
     if command == "replace" then
