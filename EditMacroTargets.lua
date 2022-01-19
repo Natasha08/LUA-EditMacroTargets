04ffda77b31590bc9036b2b54a8e1907e03d50b2
natasha08 <admin@natashaosborne.com>
2022-01-18 18:15:59 -0700

    use libraries to lock emt when in combat; cleanup

--- EditMacroTargets.lua
+++ EditMacroTargets.lua
@@ -11,18 +11,24 @@ SLASH_EditMacroTargets2 = "/editmacrotargets";
   -- on button click
   -- when using the replace method
 -- configure help tips
--- update slash commands (bet or betm, and when it is written out in long form)
--- is there a better way to get the first index of the macros, or get the list to map over?
+-- style user displayed messages and warnings
 -- refactor/cleanup
 
+local LibOO = LibStub("LibOO-1.0", true);
+local EMT = CreateFrame("Frame", "EMT_TargetFrame", UIParent, "EtherealFrameTemplate");
+local INITIAL_MACRO_INDEX = 1;
+_G.EMT = LibStub("AceAddon-3.0"):NewAddon(EMT, "EditMacroTargets", "AceEvent-3.0", "AceTimer-3.0", "AceConsole-3.0", "AceComm-3.0", "AceSerializer-3.0");
+_G.EditMacroTargets = _G.EMT;
+local EMT = _G.EMT;
+EMT.locked = false;
 
 local function UpdateMacro(from, to, previousIndex)
     if (MacroFrame and MacroFrame:IsShown()) then HideUIPanel(MacroFrame);end
-    currentIndex = previousIndex;
+    currentIndex = previousIndex or INITIAL_MACRO_INDEX;
     name, icon, body, isLocal = GetMacroInfo(currentIndex);
 
-    at = string.gsub("@name", "name", from)
-    target = string.gsub("target=name", "name", from)
+    at = string.gsub("@name", "name", from);
+    target = string.gsub("target=name", "name", from);
 
 
     if body:find(at) or body:find(target) then
@@ -47,52 +53,61 @@ local function UpdateMacro(from, to, previousIndex)
 end
 
 local function DisplayModal()
-    local Modal = CreateFrame("Frame", "EMT_TargetFrame", UIParent, "EtherealFrameTemplate");
+    EMT:SetSize(300, 360);
+    EMT:SetPoint("CENTER", UIParent, "CENTER");
+    EMT:SetFrameLevel(100)
 
-    Modal:SetSize(300, 360);
-    Modal:SetPoint("CENTER", UIParent, "CENTER");
-    Modal:SetFrameLevel(100)
+    EMT.title = EMT:CreateFontString(nil, "OVERLAY");
+    EMT.title:SetFontObject("GameFontHighlight");
+    EMT.title:SetPoint("CENTER", EMT.TitleBg, "CENTER", 5, 0);
+    EMT.title:SetText("Bulk Edit Target Options");
 
-    Modal.title = Modal:CreateFontString(nil, "OVERLAY");
-    Modal.title:SetFontObject("GameFontHighlight");
-    Modal.title:SetPoint("CENTER", Modal.TitleBg, "CENTER", 5, 0);
-    Modal.title:SetText("Bulk Edit Target Options");
-
-    Modal.saveButton = CreateFrame("Button", "saveButton", Modal, "EMT_ButtonTemplate");
-    Modal.saveButton:SetPoint("CENTER", Modal, "Bottom", 0, 70);
-    Modal.saveButton:SetSize(70, 30);
-    Modal.saveButton:SetText("Save");
-    Modal.saveButton:SetNormalFontObject("GameFontNormalLarge");
-    Modal.saveButton:SetHighlightFontObject("GameFontHighlightLarge");
-    Modal.saveButton:SetScript("OnClick", function(self)
-        from = Modal.editInput1:GetText();
-        to = Modal.editInput2:GetText();
-        UpdateMacro(from, to, 121);
+    EMT.saveButton = CreateFrame("Button", "saveButton", EMT, "EMT_ButtonTemplate");
+    EMT.saveButton:SetPoint("CENTER", EMT, "Bottom", 0, 70);
+    EMT.saveButton:SetSize(70, 30);
+    EMT.saveButton:SetText("Save");
+    EMT.saveButton:SetNormalFontObject("GameFontNormalLarge");
+    EMT.saveButton:SetHighlightFontObject("GameFontHighlightLarge");
+    EMT.saveButton:SetScript("OnClick", function(self)
+        from = EMT.editInput1:GetText();
+        to = EMT.editInput2:GetText();
+        UpdateMacro(from, to);
     end)
 
-    Modal.editInput1 = CreateFrame("EditBox", "FromInput", Modal, "EMT_EditBoxTemplate");
-    Modal.editInput1:SetPoint("LEFT", Modal, "TOP", -60, -100);
-    Modal.editInput1:SetWidth(180);
-    Modal.editInput1:SetHeight(40);
-    Modal.editInput1:SetMovable(false);
-    Modal.editInput1:SetAutoFocus(true);
-    Modal.editInput1:SetMaxLetters(100);
+    EMT.editInput1 = CreateFrame("EditBox", "FromInput", EMT, "EMT_EditBoxTemplate");
+    EMT.editInput1:SetPoint("LEFT", EMT, "TOP", -60, -100);
+    EMT.editInput1:SetWidth(180);
+    EMT.editInput1:SetHeight(40);
+    EMT.editInput1:SetMovable(false);
+    EMT.editInput1:SetAutoFocus(true);
+    EMT.editInput1:SetMaxLetters(100);
 
-    Modal.editInput2 = CreateFrame("EditBox", "ToInput", Modal, "EMT_EditBoxTemplate");
-    Modal.editInput2:SetPoint("LEFT", Modal,  "TOP", -60, -130);
-    Modal.editInput2:SetWidth(180);
-    Modal.editInput2:SetHeight(40);
-    Modal.editInput2:SetMovable(false);
-    Modal.editInput2:SetAutoFocus(false);
-    Modal.editInput2:SetMaxLetters(100);
+    EMT.editInput2 = CreateFrame("EditBox", "ToInput", EMT, "EMT_EditBoxTemplate");
+    EMT.editInput2:SetPoint("LEFT", EMT,  "TOP", -60, -130);
+    EMT.editInput2:SetWidth(180);
+    EMT.editInput2:SetHeight(40);
+    EMT.editInput2:SetMovable(false);
+    EMT.editInput2:SetAutoFocus(false);
+    EMT.editInput2:SetMaxLetters(100);
 end
 
-SlashCmdList["EditMacroTargets"] = function(msg)
-    local command, names = msg:match("^(%S*)%s*(.-)$");
+EMT:RegisterEvent("PLAYER_REGEN_DISABLED", function()
+    EMT.locked = true;
+end)
 
-    if command == "replace" then
-        local from, to = names:match("^(%S*)%s*(.-)$");
-        UpdateMacro(from, to, 121);
+EMT:RegisterEvent("PLAYER_REGEN_ENABLED", function()
+    EMT.locked = false;
+end)
+
+SlashCmdList["EditMacroTargets"] = function(msg)
+    if UnitIsDeadOrGhost("player") or EMT.locked then
+       return print("EditMacroTargets is locked while dead or in combat.");
+    end
+
+    local from, to = msg:match("^(%S*)%s*(.-)$");
+
+    if from ~= nil and to ~= nil then
+        UpdateMacro(from, to);
     end
 
     if msg == "" then
